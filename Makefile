63909c46587c9d8c5a0a7c7e1c55681d4de43c0c
Andrew Chilton <andychilton@gmail.com>
2009-12-06 10:20:06 +1300

    Add a more versatile deploy.sh script

--- Makefile
+++ Makefile
@@ -1,31 +1,6 @@
 start-server:
 	../google_appengine/dev_appserver.py --datastore_path=store/data.db --history_path=store/data.db.history ./
 
-upload:
-	../google_appengine/appcfg.py update ./
-
-update-indexes:
-	../google_appengine/appcfg.py update_indexes ./
-
-# for when you delete an index (to clear it out)
-vacuum-indexes:
-	../google_appengine/appcfg.py vacuum_indexes ./
-
-# update task queues
-update-queues:
-	../google_appengine/appcfg.py update_queues ./
-
-# update cron
-update-cron:
-	../google_appengine/appcfg.py update_cron ./
-
-# download log for today (check appcfg.py for further info)
-request-logs:
-	../google_appengine/appcfg.py request_logs ./ log.txt
-
-commit:
-	git commit -m 'Update of generated files' index.yaml
-
 issue-summary:
 	cil summary --is-open --label=Milestone-v0.1
 
---
+++ deploy.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo 'Please provide an app to deploy'
+    exit 2
+fi
+
+if [ -z "$2" ]; then
+    echo 'Please specify a command:'
+    echo ' - update'
+    echo ' - update_indexes'
+    echo ' - vacuum_indexes'
+    echo ' - update_queues'
+    echo ' - update_cron'
+    exit 2
+fi
+
+APP="$1"
+COMMAND="$2"
+
+echo "Performing '$COMMAND' on '$APP'"
+
+case "$APP" in
+    chilts)
+        echo "Hi Chilts"
+        APP=website-chilts-org
+        ;;
+    lollysite)
+        echo "Main Lollysite site"
+        ;;
+    *)
+       echo "Unknown site"
+       exit 2
+       ;;
+esac
+
+# setup the app.yaml for this application
+sed -i.orig "1,1s/^application: lollysite$/application: $APP/" app.yaml
+
+case "$COMMAND" in
+    update|update_indexes|update_queues|update_cron)
+        echo "Doing $COMMAND"
+        ../google_appengine/appcfg.py $COMMAND ./
+        ;;
+    request-logs)
+        echo "Doing update"
+        ../google_appengine/appcfg.py request_logs ./ logs/access-$APP.log
+        ;;
+    *)
+        echo "Unknown command"
+        ;;
+esac
+
+# put the normal app.yaml back
+sed -i.last "1,1s/^application: .*$/application: lollysite/" app.yaml
